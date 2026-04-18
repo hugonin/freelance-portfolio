ff776fb0bd831c18aa9d5454310a667bcc9763a4
hugonin <hugonin@free.fr>
2026-04-18 18:31:09 +0200

    updated the masterPage file

--- src/pages/masterPage.js
+++ src/pages/masterPage.js
@@ -1,20 +1,13 @@
 import wixWindowFrontend from 'wix-window-frontend';
 
-$w.onReady(function () {
-    console.log("Velo: masterPage ready");
-
-    // On attend que la page soit réellement chargée aux yeux de l'utilisateur
-    // 1000ms est une valeur sûre pour un portfolio avec des images
-    setTimeout(() => {
-        const loader = $w('#customPreloader');
-
-        if (loader) {
-            setTimeout(() => {
-                loader.setAttribute('status', 'done');
-                // Optionnel : Animation d'entrée du contenu
-                $w('Document').scrollTo(0, 0); // Force le scroll en haut
-            }, 800);
-
-
-        }
-    });
+// masterPage.js
+$w.onReady(() => {
+    const loader = $w('#customPreloader');
+    if (loader) {
+        setTimeout(() => {
+            loader.setAttribute('status', 'done');
+            // Optionnel : Animation d'entrée du contenu
+            $w('Document').scrollTo(0,0); // Force le scroll en haut
+        }, 800);
+    }
+});
